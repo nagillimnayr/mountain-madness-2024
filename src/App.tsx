7a4c965feb1e0ce6235d1cfdc84a222ef7eb9751
Ryan <ryanmilligan88@gmail.com>
2024-03-23 14:55:29 -0700

    modify layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ function App() {
 
   return (
     <>
-      <div className='w-screen h-screen flex flex-col justify-center items-center  border-2 border-blue-500'>
-        <h1 className='text-4xl font-extrabold w-fit h-fit rounded-md p-2 text-red-500'>
-          Hello, Mountain Madness 2024!
-        </h1>
-        <div className='w-96 h-fit rounded-md flex flex-row justify-center items-center p-2'>
+      <div className='w-screen h-screen flex flex-col justify-start items-center  border-2 border-blue-500 gap-16 p-16'>
+        <div className='flex flex-col justify-center items-center'>
+          <h2 className='text-4xl font-extrabold w-fit h-fit rounded-md p-2 text-red-500'>
+            Hello, Mountain Madness 2024!
+          </h2>
           <h3 className='text-2xl font-extrabold text-green-500'>
             A New Perspective!
           </h3>
--- src/components/upload/InputImage.tsx
+++ src/components/upload/InputImage.tsx
@@ -1,6 +1,4 @@
 
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 
 export const InputImage = () => {
   
