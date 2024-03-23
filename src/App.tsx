17f0ebbe21940dfd063051b32d0a2b23d3375d31
Ryan <ryanmilligan88@gmail.com>
2024-03-23 11:38:19 -0700

    add some text

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ function App() {
 
   return (
     <>
-      <div className='w-screen h-screen flex flex-row justify-center items-center  border-2 border-blue-500'>
-        <h1 className='text-xl font-extrabold w-fit h-fit border-2 border-red-500 rounded-md p-2 text-red-500'>Hello, Vite!</h1>
+      <div className='w-screen h-screen flex flex-col justify-center items-center  border-2 border-blue-500'>
+        <h1 className='text-4xl font-extrabold w-fit h-fit rounded-md p-2 text-red-500'>
+          Hello, Mountain Madness 2024!
+        </h1>
+        <div className='w-96 h-fit rounded-md flex flex-row justify-center items-center p-2'>
+          <h3 className='text-2xl font-extrabold text-green-500'>
+            A New Perspective!
+          </h3>
+        </div>
       </div>
       
     </>
