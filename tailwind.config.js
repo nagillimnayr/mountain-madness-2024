c48cd5d1001116a5320aa528b259e43738541859
Ryan <ryanmilligan88@gmail.com>
2024-03-23 10:27:59 -0700

    install postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx,md,mdx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx,md,mdx}"
+  ],
   theme: {
     extend: {},
   },
