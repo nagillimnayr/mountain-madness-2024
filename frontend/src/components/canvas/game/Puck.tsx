2dc539dbea11f46d39875697ef8faf21d72f60d8
Ryan Milligan <ryanmilligan88@gmail.com>
2024-03-23 17:45:44 -0700

    make puck move in circle

--- frontend/src/components/canvas/game/Puck.tsx
+++ frontend/src/components/canvas/game/Puck.tsx
@@ -1,6 +1,8 @@
 import { PUCK_RADIUS, PUCK_THICKNESS } from '@/lib/constants';
 import { Cylinder } from '@react-three/drei'
-import { Vector3Tuple } from 'three';
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { Object3D, Vector3Tuple } from 'three';
 
 /* 
   Air hockey pucks
@@ -14,11 +16,26 @@ type PuckProps = {
 
 }
 export const Puck = ({position} : PuckProps) => {
+
+  const puckRef = useRef<Object3D>(null!);
+
+  const timer = useRef(0);
+
+  useFrame((state, delta) => {
+    timer.current += delta;
+    const x = Math.cos(timer.current);
+    const z = Math.sin(timer.current);
+    puckRef.current.position.set(x, 0, z);
+
+  });
+
   return (
-    <Cylinder 
-      args={[PUCK_RADIUS, PUCK_RADIUS, PUCK_THICKNESS]} 
-      position={position}>
-      <meshStandardMaterial color={'gray'} />
-    </Cylinder>
+    <object3D ref={puckRef}>
+      <Cylinder 
+        args={[PUCK_RADIUS, PUCK_RADIUS, PUCK_THICKNESS]} 
+        position={position}>
+        <meshStandardMaterial color={'gray'} />
+      </Cylinder>
+    </object3D>
   )
 }
