e3bd9cb078cfe7f39bf79139dd4b9752447d1ee4
Ryan Milligan <ryanmilligan88@gmail.com>
2024-03-23 19:24:40 -0700

    add overlay that disappears on click

--- frontend/src/components/dom/user-interface/UserInterface.tsx
+++ frontend/src/components/dom/user-interface/UserInterface.tsx
@@ -1,10 +1,25 @@
+import { useCallback, useRef } from 'react';
 
 export const UserInterface = () => {
+  const containerRef = useRef<HTMLDivElement>(null!);
+
+  const handleClick = useCallback(() => {
+    containerRef.current.dataset.blend = "false";
+  }, []);
+
   return (
-    <div className='absolute top-0 left-0 w-full h-screen border-8 border-red-500 pointer-events-none'>
-      <div className='w-full h-full flex items-center justify-center'>
-        <h2 className='text-2xl font-extrabold text-white'>Hello, World!</h2>
-      </div>
+    <div 
+      className='absolute top-0 left-0 w-full h-screen border-8 border-red-500 pointer-events-none'
+      onClick={handleClick}
+      >
+        <div 
+        ref={containerRef}
+        data-blend="true"
+        className='w-full h-full bg-transparent data-[blend=true]:bg-gray-300 pointer-events-auto'>
+          <div className='w-full h-full flex items-center justify-center'>
+            <h2 className='text-2xl font-extrabold text-white'>Hello, World!</h2>
+          </div>
+        </div>
     </div>
   )
 }
--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -7,3 +7,9 @@ export const TABLE_HEIGHT = 0.07;
 
 export const PUCK_THICKNESS = 0.007;
 export const PUCK_RADIUS = 0.040;
+
+
+export const PADDLE_BASE_RADIUS = 0.048;
+export const PADDLE_BASE_THICKNESS = 0.019;
+export const PADDLE_HANDLE_RADIUS = PADDLE_BASE_RADIUS / 3;
+export const PADDLE_HANDLE_HEIGHT = 0.060;
