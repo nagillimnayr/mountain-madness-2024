e92cbaf0e8184802790073cf4047dc1cdfca5cdc
Ryan <ryanmilligan88@gmail.com>
2024-03-23 17:03:42 -0700

    add constants for table dimensions

--- src/components/canvas/game/Table.tsx
+++ src/components/canvas/game/Table.tsx
@@ -1,9 +1,13 @@
 import { Box } from '@react-three/drei';
 
+const TABLE_WIDTH = 4;
+const TABLE_LENGTH = 7;
+const TABLE_HEIGHT = 0.2;
+
 export const Table = () => {
   
   return (
-    <Box args={[4, 0.2, 7]}>
+    <Box args={[TABLE_WIDTH, TABLE_HEIGHT, TABLE_LENGTH]}>
       <meshStandardMaterial color={'red'} />
     </Box>
   )
